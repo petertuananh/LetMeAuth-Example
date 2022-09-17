c7d80fc3ce9f26734ef30ec3c0cc8528e8318378
petertadev <petertuananh@hotmail.com>
2022-09-17 13:47:49 +0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const app = express()
 const axios = require("axios")
-const config = {
-    port : 1000,
-    application_id : "23987273239305902",
-    redirect : 'http://localhost:1000/callback'
-}
-app.get("/", async (req, res) => {
-    res.sendFile(process.cwd() + '/index.html')
+var passport = require('passport');
+var passportLocal = require('passport-local');
+var expressSession = require('express-session');
+const cookieParser = require('cookie-parser');
+const config = require("./config.json")
+app.use(expressSession({ secret: '#@*#&%#%^%#$##@*#&%#%^%#$##@*#&%#%^%#$#' }))
+app.use(passport.session());
+app.set('view engine', 'ejs');
+app.use(cookieParser())
+
+
+app.get("/", checkAuth, async (req, res) => {
+    res.json(req.user)
 })
 app.get("/login", async (req, res) => {
-    res.redirect(`https://authme.yourauth.xyz/oauth2/authorize?app_id=${config.application_id}&redirect_url=${config.redirect}`)
+    res.redirect(`https://letmeauth.xyz/oauth2/authorize?app_id=${config.app_id}&redirect_url=${config.callback_url}`)
 })
 app.get("/callback", async (req, res) => {
-    const results = await axios.default.get(`https://authme.yourauth.xyz/oauth2/checktoken?token=${req.query.token}&app_id=${config.application_id}`)
-    return res.json(results.data)
+    res.cookie("token", req.query.token)
+    return res.redirect(req.cookies.redirect ? req.cookies.redirect : '/')
 })
-app.listen(config.port)
+async function checkAuth(req, res, next) {
+    const data = await axios.default.get(`https://letmeauth.xyz/oauth2/checktoken?app_id=${config.app_id}&token=${req.cookies.token}`)
+    if (data.data.id) {
+        req.user = data.data
+        return next()
+    }
+}
+app.listen(3000)
